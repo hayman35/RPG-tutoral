5c60ffc259e8e3efb75830f69c45e016d15d8b3d
Hayman Abdeljabbar <habdelj@uwo.ca>
2021-03-02 20:55:48 -0500

    Init Upload

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using RPG.Movement;
+using RPG.Camera;
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+{
+    Mover move;
+
+    private void Start() 
+    {
+        move = GetComponent<Mover>();
+    }
+    private void Update() 
+    {
+        if (Input.GetMouseButton(0)) 
+        {
+            MoveToCursor();
+        }
+    }
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            move.MoveTo(hit.point);
+        }
+    }
+}
+}
+
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAnimator();        
+    }
+
+    private void UpdateAnimator()
+    {
+        var velocity = agent.velocity;
+        var localVelocity = transform.InverseTransformDirection(velocity);
+        var speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed",speed);
+    }
+
+    public void MoveTo(Vector3 dest)
+    {
+        agent.SetDestination(dest);
+    }
+}
+}
+
